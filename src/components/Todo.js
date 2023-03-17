809a1b2a5c1fb2080b5e214ec6ece0369d158dfb
viktorija-verebes <viki.verebes90@gmail.com>
2023-03-17 15:12:16 +0100

    event props

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -11,14 +11,18 @@ function Todo(props) {
         setModalIsOpen(true);
     }
 
+    function closeModalHandler() {
+        setModalIsOpen(false);
+    }
+
     return (
          <div className="card">
             <h2>{props.text}</h2>
             <div className="actions">
                 <button className="btn" onClick={deleteHandler}>Delete</button>
             </div>
-            {modalIsOpen ? <Modal /> : null}
-            { modalIsOpen ? <Backdrop /> : null }
+            {modalIsOpen ? <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} /> : null}
+            { modalIsOpen ? <Backdrop onClick={closeModalHandler} /> : null }
         </div>
     );
 }
