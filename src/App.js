7d8dbe4444f912842a16b59239dd50d77cea162b
viktorija-verebes <viki.verebes90@gmail.com>
2023-03-17 13:53:02 +0100

    props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import Todo from './components/Todo';
 
 function App() {
-  return <div>Hello!</div>
+  return <div>
+    <h1>My Todos</h1>
+    <Todo text="learn 1" />
+    <Todo text="learn 2" />
+    <Todo text="learn 3" />
+  </div>
 }
 
 export default App;
